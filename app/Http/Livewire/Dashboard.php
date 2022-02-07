d3f34292516fde134a726eb1660435707949f73f
Ivan <ivan.kovacevic1996@gmail.com>
2022-02-07 13:54:48 +0100

    ++ Ocistio nepotrebne filove ++ Dodo na DASHBOARD test za LOG da vidim da se sprema u bazu i da sve radi

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -7,12 +7,7 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Comment;
-use App\Models\Partner;
-use App\Models\PastTask;
-use App\Models\PastTaskWorker;
-use App\Models\Task;
-use App\Models\Worker;
+
 use Carbon\Carbon;
 use Debugbar;
 use Illuminate\Database\QueryException;
@@ -21,6 +16,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Response;
 use Livewire\Component;
+use Spatie\Activitylog\Models\Activity;
 use function Symfony\Component\String\b;
 
 class Dashboard extends Component
@@ -29,9 +25,18 @@ class Dashboard extends Component
     public $selectedWorkers = [];
     public $comment;
 
+    public function testActivityLog(){
+        activity()->log('LOUG!');
+    }
+
+    public function mount()
+    {
+
+    }
 
     public function render()
     {
-        return view('livewire.dashboard');
+        $activites = Activity::all();
+        return view('livewire.dashboard', ['activites' => $activites]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
-use App\Http\Controllers\AddNewServiceWizardController;
-use App\Http\Controllers\EditRadniciController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\SuperAdminDashboardController;
-use App\Http\Controllers\WorkersController;
-use App\Http\Controllers\PartnersController;
 use App\Http\Controllers\EditUserController;
-use App\Http\Livewire\CommentsOverview;
 use App\Http\Livewire\Dashboard;
-use App\Http\Livewire\EditAddPartner;
-use App\Http\Livewire\EditAddWorker;
-use App\Http\Livewire\PartnersOverview;
-use App\Http\Livewire\PastTasksOverview;
-use App\Http\Livewire\Statistics;
-use App\Http\Livewire\TasksOverview;
-use App\Http\Livewire\WorkersOverview;
 use Illuminate\Support\Facades\Route;
 
 /*
