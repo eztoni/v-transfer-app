1ab44a631203d76529dbea08a748a453712ff4bf
Tin <modrictin7@gmail.com>
2022-02-22 20:55:24 +0100

    Trait

--- app/Traits/FieldMask.php
+++ app/Traits/FieldMask.php
@@ -3,18 +3,21 @@
 namespace App\Traits;
 
 use App\Models\User;
+use Illuminate\Support\Facades\App;
 
 trait FieldMask
 {
 
     public function getAttribute($name)
     {
-        if (\Auth::user()->hasRole(User::ROLE_SUPER_ADMIN)) {
-            if (property_exists($this, 'masked')) {
-                if (is_array($this->masked)) {
-                    foreach ($this->masked as $fieldName) {
-                        if ($name == $fieldName) {
-                            return \Str::mask(parent::getAttribute($name), '*', 3);
+        if (App::environment('production')) {
+            if (\Auth::user()->hasRole(User::ROLE_SUPER_ADMIN)) {
+                if (property_exists($this, 'masked')) {
+                    if (is_array($this->masked)) {
+                        foreach ($this->masked as $fieldName) {
+                            if ($name == $fieldName) {
+                                return \Str::mask(parent::getAttribute($name), '*', 3);
+                            }
                         }
                     }
                 }
