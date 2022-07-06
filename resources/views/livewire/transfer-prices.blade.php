02595da5a78f3f6aea0b879bbcb124e9dfbb3f26
KovaIvan <ivan.kovacevic1996@gmail.com>
2022-07-06 16:12:21 +0200

    ++ TRANSFER EDIT WIRE UI

--- resources/views/livewire/transfer-prices.blade.php
+++ resources/views/livewire/transfer-prices.blade.php
@@ -1,129 +1,123 @@
-<div>
+<x-card title="Transfer Prices">
 
-    @if($this->showSearch)
+    <x-slot name="action" >
 
-        <x-ez-card class="mb-4">
+        @if($this->showSearch)
+            <x-native-select
+                label="Transfers:"
+                placeholder="Select a transfer"
+                option-label="name"
+                option-value="id"
+                :options="$transfers->map(fn ($m) => ['id'=>$m->id,'name'=>$m->name])->toArray() "
+                wire:model="transferId"
+            />
+        @endif
 
-            <x-slot name="title" class="flex justify-between">
-                <span>Transfers <i class="fas fa-car-alt"></i></span>
-            </x-slot>
-
-            <x-slot name="body">
-
-                <div class="form-control">
-
-                    <label class="label">
-                        <span class="label-text">Select a transfer:</span>
-                    </label>
-                    <select wire:model="transferId" class="select select-bordered">
-                        <option value="">Select a transfer</option>
-                        @foreach($transfers as $t)
-                            <option value="{{$t->id}}">{{$t->name}}</option>
-                        @endforeach
-                    </select>
-
-                    @error('transferId')
-                    <x-input-alert type='warning'>{{$message}}</x-input-alert>
-                    @enderror
-                </div>
-
-            </x-slot>
-        </x-ez-card>
-
-    @endif
-
-    @if ($transferId)
-        <x-ez-card>
-
-            <x-slot name="title" class="flex justify-between">
-              <div class="">
-                  Transfer Prices
-              </div>
-                <div class="">
-                    <label class="label">
-                        <span class="label-text">Partner:</span>
-                    </label>
-                    <select class="my-select" wire:model="partnerId">
-
-                        @foreach(\App\Models\Partner::all() as $partner)
-                            <option value="{{$partner->id}}">{{$partner->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </x-slot>
-
-            <x-slot name="body">
-
-                @if ($this->routes->isNotEmpty())
+        @if ($transferId)
+                <x-native-select
+                    label="Partner:"
+                    option-label="name"
+                    option-value="id"
+                    :options="\App\Models\Partner::all()->map(fn ($m) => ['id'=>$m->id,'name'=>$m->name])->toArray() "
+                    wire:model="transferId"
+                />
+        @endif
 
 
-                        <table class="table table-compact">
-                            <!-- head -->
-                            <thead>
-                            <tr>
-                                <th>#ID</th>
-                                <th>Route name</th>
-                                <th>From</th>
-                                <th>To</th>
-                                <th class="text-center">Round Trip</th>
-                                <th>Round Trip Price (EUR)</th>
-                                <th class="text-right pr-8">Price (EUR)</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($this->routes as $r)
-                                <tr x-data="{dirty:{{'true'}} }"  wire:key="{{$r->id}}">
-                                    <th>#{{$r->id}}</th>
-                                    <td>{{$r->name}}</td>
-                                    <td>{{$r->startingPoint->name}}</td>
-                                    <td>{{$r->endingPoint->name}}</td>
-                                    <td class="text-center"><input wire:click="saveRoundTrip({{$r->id}})" wire:model="routeRoundTrip.{{$r->id}}" @if(!empty($routeRoundTrip[$r->id])) checked @endif type="checkbox" class="checkbox checkbox-primary"></td>
-                                    <td class="text-right">
-                                        <div class="form-control">
-                                            <div class="input-group justify">
-                                                <input wire:model="routePriceRoundTrip.{{$r->id}}" @if(empty($routeRoundTrip[$r->id])) disabled @endif placeholder="Price" class=" @error('routePriceRoundTrip.'.$r->id) input-error @enderror input input-sm input-bordered">
-                                                <button wire:click="saveRoutePriceRoundTrip({{$r->id}})" @if(empty($routeRoundTrip[$r->id])) disabled @endif class="btn btn-sm  btn-success">
-                                                    Save
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="text-right">
-                                        <div class="form-control ">
-                                            <div class="input-group justify-end">
-                                                <input wire:model="routePrice.{{$r->id}}" placeholder="Price" @keyup="dirty=false" class="@error('routePrice.'.$r->id) input-error @enderror input input-sm input-bordered">
-                                                <button wire:click="saveRoutePrice({{$r->id}})"  :disabled="dirty"  class="btn btn-sm  btn-success">
-                                                    Save
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </td>
+    </x-slot>
+
+    <table class="ds-table ds-table-compact w-full" wire:loading.delay.class="opacity-50">
+        <thead>
+        <tr>
+            <th>#ID</th>
+            <th>Route name</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Round Trip</th>
+            <th>Round Trip Price (EUR)</th>
+            <th class="text-right pr-8">Price (EUR)</th>
+        </tr>
+        </thead>
+        <tbody>
 
 
-                                </tr>
-                            @endforeach
+        @if ($transferId)
 
-                            </tbody>
-                        </table>
+            @if ($this->routes->isNotEmpty())
+                @forelse ($this->routes as $r)
 
-                @else
-                    No routes for this transfer, add new route!
-                @endif
+                    <tr x-data="{dirty:{{'true'}} }"  wire:key="{{$r->id}}">
+                        <th>#{{$r->id}}</th>
+                        <td>{{$r->name}}</td>
+                        <td>{{$r->startingPoint->name}}</td>
+                        <td>{{$r->endingPoint->name}}</td>
+                        <td>
+                            <x-toggle wire:click="saveRoundTrip({{$r->id}})" wire:model="routeRoundTrip.{{$r->id}}" />
+                        </td>
+                        <td>
+                            <div class="form-control">
+                                <div class="input-group justify">
+
+                                    <input wire:model="routePriceRoundTrip.{{$r->id}}" @if(empty($routeRoundTrip[$r->id])) disabled @endif placeholder="Price" class=" @error('routePriceRoundTrip.'.$r->id) ds-input-error @enderror ds-input ds-input-sm ds-input-bordered">
+
+                                    @if(!empty($routeRoundTrip[$r->id]))
+                                    <x-button sm wire:click="saveRoutePriceRoundTrip({{$r->id}})" label="Save" positive/>
+                                    @endif
+
+                                </div>
+                            </div>
+                        </td>
+                        <td class="text-right">
+                            <div class="form-control ">
+                                <div class="input-group justify-end">
+                                    <input wire:model="routePrice.{{$r->id}}" placeholder="Price" @keyup="dirty=false" class="@error('routePrice.'.$r->id) input-error @enderror input input-sm input-bordered">
+                                    <x-button  wire:click="saveRoutePrice({{$r->id}})" label="Save" positive/>
+                                </div>
+                            </div>
+                        </td>
 
 
-            </x-slot>
+                    </tr>
 
-        </x-ez-card>
-    @else
-            <x-ez-card>
-                <x-slot name="body">
-                    Select a transfer to add prices!
-                </x-slot>
-            </x-ez-card>
-    @endif
+                @empty
+                    <tr>
+                        <td colspan="999">
+                            <div class="ds-alert ds-alert-warning">
+                                <div class="flex-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         class="w-6 h-6 mx-2 stroke-current">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
+                                    </svg>
+                                    <label>No defined extras</label>
+                                </div>
+                            </div>
+                        </TD>
+                    </tr>
+                @endforelse
+            @else
+                No routes for this transfer, add new route!
+            @endif
+        @else
+            <tr>
+                <td colspan="999">
+                    <div class="ds-alert ds-alert-warning">
+                        <div class="flex-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 class="w-6 h-6 mx-2 stroke-current">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
+                            </svg>
+                            <label>Select a transfer to add prices!</label>
+                        </div>
+                    </div>
+                </TD>
+            </tr>
+        @endif
+        </tbody>
+
+    </table>
 
 
-
-
-</div>
+</x-card>
 
