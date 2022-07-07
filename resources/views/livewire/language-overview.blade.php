8b5e9fb5f685e4f058b027ba2f472b94af7ff5d6
KovaIvan <ivan.kovacevic1996@gmail.com>
2022-07-07 14:24:58 +0200

    ++ Company Overview + Language Overview WIRE UI

--- resources/views/livewire/language-overview.blade.php
+++ resources/views/livewire/language-overview.blade.php
@@ -1,124 +1,106 @@
 <div>
 
-    <x-ez-card>
-        <x-slot name="title" class="flex justify-between">
-            Languages
-
-            <button wire:click="addLanguage" class="btn btn-sm ">Add Language</button>
-
+    <x-card title="Languages">
+        <x-slot name="action">
+            <x-button wire:click="addLanguage()" primary label="Add Language"/>
         </x-slot>
-        <x-slot name="body">
 
-            <input type="text" wire:model="search" class="input input-primary my-2" placeholder="Find Language">
-            <table class="table table-compact w-full" wire:loading.delay.class="opacity-50">
-                <thead>
+
+        <x-input label="Find language" wire:model="search"></x-input>
+
+        <table class="ds-table ds-table-compact w-full" wire:loading.delay.class="opacity-50">
+            <thead>
+            <tr>
+                <th>#Id</th>
+                <th>Name</th>
+                <th>ISO 639-1</th>
+                <th class="text-right pr-4">Update</th>
+                <th class="text-right pr-4">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse ($languages as $l)
                 <tr>
-                    <th>#Id</th>
-                    <th>Name</th>
-                    <th>ISO 639-1</th>
-                    <th class="text-center">Update</th>
-                    <th class="text-right"><span class="pr-4">Delete</span></th>
-
+                    <th>{{ $l->id }}</th>
+                    <th>{{ $l->name }}</th>
+                    <th>{{ $l->language_code }}</th>
+                    <td class="text-right">
+                        <x-button
+                            wire:click="updateLanguage({{$l->id}})"
+                            rounded
+                            primary icon="pencil"
+                            target="_blank"
+                        />
+                    </td>
+                    <td class="text-right">
+                        <x-button
+                            wire:click="openSoftDeleteModal({{$l->id}})"
+                            rounded
+                            rose icon="trash"
+                            target="_blank"
+                        />
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                @forelse ($languages as $l)
-
-                    <tr>
-                        <th>{{ $l->id }}</th>
-                        <th >{{ $l->name }}</th>
-                        <th >{{ $l->language_code }}</th>
-                        <td class="text-center">
-                            <button wire:click="updateLanguage({{$l->id}})" class="btn btn-sm btn-success">
-                                Update
-                            </button>
-                        </td>
-                        <td class="text-right">
-                            <button wire:click="openSoftDeleteModal({{$l->id}})" class="btn btn-sm btn-ghost">
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-
-                @empty
-                    <tr>
-                        <td colspan="999">
-                            <div class="ds-alert ds-alert-warning">
-                                <div class="flex-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         class="w-6 h-6 mx-2 stroke-current">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
-                                    </svg>
-                                    <label>No defined languages</label>
-                                </div>
+            @empty
+                <tr>
+                    <td colspan="999">
+                        <div class="alert alert-warning">
+                            <div class="flex-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     class="w-6 h-6 mx-2 stroke-current">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
+                                </svg>
+                                <label>No defined languages</label>
                             </div>
-                        </TD>
-                    </tr>
-                @endforelse
-                </tbody>
+                        </div>
+                    </TD>
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
+        {{$languages->links()}}
+    </x-card>
 
-            </table>
+    <x-modal.card blur wire:model.defer="languageModal" title="{{$this->language->id > 0 ? 'Update':'Add'}} Language">
+        <div class="form-control mb-4">
+            <x-input label="Name:" wire:model.defer="language.name" />
+        </div>
 
+        <div class="form-control mb-4">
+            <x-input label="Language Code:" wire:model.defer="language.language_code" placeholder="ISO 639-1 code"/>
+        </div>
 
-            <div class="modal {{ $softDeleteModal ? 'modal-open fadeIn' : '' }}">
-                <div class="modal-box max-h-screen overflow-y-auto">
-                    <b>Confirm deletion?</b>
-                    <p>This action will delete the language.</p>
-                    <hr class="my-4">
+        <x-errors />
 
-                    <div class="mt-4 flex justify-between">
-                        <button wire:click="closeSoftDeleteModal()" class="btn btn-sm ">Close</button>
-                        <button wire:click="softDelete()" class="btn btn-sm ">Delete</button>
-                    </div>
-                </div>
+        <x-slot name="footer">
+            <div class="float-right">
+                <x-button wire:click="closeLanguageModal" label="Close" rose/>
+                <x-button wire:click="saveLanguageData" label="Save" positive/>
             </div>
 
+        </x-slot>
+    </x-modal.card>
 
-            <div class="modal {{ $languageModal ? 'modal-open fadeIn' : '' }}">
-                <div class="modal-box max-h-screen overflow-y-auto">
-                    Adding new language
-                    <hr class="my-4">
+    <x-modal.card blur wire:model.defer="softDeleteModal" title="Delete Exclude - {{$this->language->name}}">
 
-                    <div class="form-control">
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Name :</span>
-                            </label>
-                            <input wire:model="language.name" class="input input-bordered"
-                                   placeholder="Name">
-                            @error('language.name')
-                            <x-input-alert type='warning'>{{$message}}</x-input-alert>
-                            @enderror
-                        </div>
+        <p>You are about to delete <b>{{$this->language->name}}</b> are you sure you want to do that?</p>
+        <p class="text-rose-500">CAREFUL - This action will delete the exclude!</p>
 
-
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Language Code :</span>
-                            </label>
-                            <input wire:model="language.language_code" class="input input-bordered"
-                                   placeholder="ISO 639-1 code">
-                            @error('language.language_code')
-                            <x-input-alert type='warning'>{{$message}}</x-input-alert>
-                            @enderror
-                        </div>
-
-
-
-
-                    </div>
-
-                    <div class="mt-4 flex justify-between">
-                        <button wire:click="closeLanguageModal()" class="btn btn-sm ">Close</button>
-                        <button wire:click="saveLanguageData()"
-                                class="btn btn-sm ">{{  !empty($this->language->exists) ? 'Update':'Add' }}</button>
-                    </div>
-                </div>
+        <x-slot name="footer">
+            <div class="float-right">
+                <x-button wire:click="closeSoftDeleteModal()" label="Close" rose/>
+                <x-button wire:click="softDelete()" label="Delete" positive/>
             </div>
 
         </x-slot>
 
-    </x-ez-card>
+    </x-modal.card>
+
 </div>
+
+
+
+
+
 
