d559c941ddd68d949e36389e9138885f84ed03e6
KovaIvan <ivan.kovacevic1996@gmail.com>
2022-07-09 13:54:37 +0200

    ++ Gallery WIRE UI + transfer edit fix

--- resources/views/livewire/image-gallery-uploader.blade.php
+++ resources/views/livewire/image-gallery-uploader.blade.php
@@ -1,8 +1,8 @@
 <div x-data="extraImages()">
 
-    <x-ez-card class="mb-4">
-        <x-slot name="body" class="px-0 pb-0 " wire:ignore>
-            <p class="px-6 mb-4">Upload up to <span class="badge">{{$this->model::MAX_IMAGES}} images</span>  for your extra. Our app optimizes  images before storing them. <br>Maximum image size is
+    <x-card cardClasses="px-0 pb-0">
+        <div wire:ignore>
+            <p class="mb-4">Upload up to <span class="badge">{{$this->model::MAX_IMAGES}} images</span>  for your extra. Our app optimizes  images before storing them. <br>Maximum image size is
                 <span class="badge">3 MB</span>.
                 We suggest using extra like <a class="link text-blue-600" href="https://tinypng.com">TinyPNG</a> for optimizing your images before storing them, so your booking engine loads faster!
             </p>
@@ -22,48 +22,41 @@
                     </div>
                 </div>
             </form>
+        </div>
+    </x-card>
 
-        </x-slot>
-    </x-ez-card>
-
-    <x-ez-card >
-
-        <x-slot name="title">
-            Images: {{$this->model->getMedia($this->mediaCollectionName)->count()}}/{{$this->model::MAX_IMAGES}}
-        </x-slot>
-
-        <x-slot name="body" class="">
-            <div class="grid  lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4">
-                @forelse($this->model->getMedia($this->mediaCollectionName) as $media )
-                    <div class=" group block py-4  " >
-                        <div class="   relative">
-                            @if($media->hasCustomProperty($this->model::IMAGE_PRIMARY_PROPERTY))
-                                <div class="badge badge-success gap-2 bg-success text-neutral-content absolute bottom-5 left-5">
-                                    Primary
-                                </div>
-                            @endif
-                            <button wire:click="delete({{$media->id}})" class="badge badge-error group-hover:opacity-100 opacity-0 bg-error text-neutral-content absolute top-2  right-2">
-                                X
-                            </button>
-                            <img src="{{$media->getFullUrl('thumb')}}" class="group-hover:rounded-b-none rounded h-44 object-cover  border border-gray-200 w-full">
-                        </div >
-                        <div class="btn-group flex group-hover:opacity-100 opacity-0 z-20 relative duration-150  transition-opacity">
-                            <button wire:click="moveLeft({{$media->id}})" class=" btn btn-accent btn-xs rounded-t-none" ><-</button>
-                            <button wire:click="makePrimary({{$media->id}})" class=" btn btn-primary flex-grow btn-xs" >Make primary</button>
-                            <button wire:click="moveRight({{$media->id}})"  class=" btn btn-accent btn-xs rounded-t-none" >-></button>
-                        </div>
-
-                    </div>
-                @empty
-                    <div class="col-span-10">
-                        <x-input-alert  type='warning'>Please upload at least one image!</x-input-alert>
-
+    <x-card cardClasses="mt-4" title="Images: {{$this->model->getMedia($this->mediaCollectionName)->count()}}/{{$this->model::MAX_IMAGES}}">
+        <div class="grid  lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4">
+            @forelse($this->model->getMedia($this->mediaCollectionName) as $media )
+                <div class=" group block py-4  " >
+                    <div class="   relative">
+                        @if($media->hasCustomProperty($this->model::IMAGE_PRIMARY_PROPERTY))
+                            <div class="badge badge-success gap-2 bg-success text-neutral-content absolute bottom-5 left-5">
+                                Primary
+                            </div>
+                        @endif
+                        <button wire:click="delete({{$media->id}})" class="badge badge-error group-hover:opacity-100 opacity-0 bg-error text-neutral-content absolute top-2  right-2">
+                            X
+                        </button>
+                        <img src="{{$media->getFullUrl('thumb')}}" class="group-hover:rounded-b-none rounded h-44 object-cover  border border-gray-200 w-full">
+                    </div >
+                    <div class="ds-btn-group flex group-hover:opacity-100 opacity-0 z-20 relative duration-150  transition-opacity">
+                        <button wire:click="moveLeft({{$media->id}})" class="ds-btn ds-btn-accent ds-btn-xs ds-rounded-t-none" ><-</button>
+                        <button wire:click="makePrimary({{$media->id}})" class=" ds-btn ds-btn-primary flex-grow ds-btn-xs" >Make primary</button>
+                        <button wire:click="moveRight({{$media->id}})"  class=" ds-btn ds-btn-accent ds-btn-xs ds-rounded-t-none" >-></button>
                     </div>
 
-                @endforelse
-            </div>
-        </x-slot>
-    </x-ez-card>
+                </div>
+            @empty
+                <div class="col-span-10">
+                    <x-input-alert  type='warning'>Please upload at least one image!</x-input-alert>
+
+                </div>
+
+            @endforelse
+        </div>
+    </x-card>
+
     @include('packages.dropzone')
     <script>
 
