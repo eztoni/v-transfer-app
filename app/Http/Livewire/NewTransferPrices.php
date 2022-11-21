2fda7da5ba5a7ccc72d7cbd0a711f8ee54e00c8f
tinmodric <modrictin7@gmail.com>
2022-11-21 20:01:11 +0100

    Mukica

--- app/Http/Livewire/NewTransferPrices.php
+++ app/Http/Livewire/NewTransferPrices.php
@@ -69,8 +69,7 @@ class NewTransferPrices extends Component
 
     public function updatedPartnerId()
     {
-        $this->partner = Partner::find($this->transferId);
-
+        $this->partner = Partner::find($this->partnerId);
         $this->setModelPrices();
     }
 
@@ -79,7 +78,6 @@ class NewTransferPrices extends Component
         $this->transfer = Transfer::find($this->transferId);
 
         $this->setModelPrices();
-
     }
 
 
@@ -87,7 +85,7 @@ class NewTransferPrices extends Component
     {
 
         if ($this->transfer && $this->partner) {
-
+            ray($this->partner->id);
             $this->modelPrices = TransferPricePivot::query()
                 ->where('transfer_id', $this->transfer->id)
                 ->where('partner_id', $this->partner->id)
