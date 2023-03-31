f98b64cfec62a650e76941d01e52594dce5a1015
Toni Njiric <toni.njiric@ez-booker.com>
2023-03-31 13:48:58 +0200

    Update GetMailHeaderAddressAndName.php

--- app/Actions/Mail/GetMailHeaderAddressAndName.php
+++ app/Actions/Mail/GetMailHeaderAddressAndName.php
@@ -5,11 +5,14 @@ namespace App\Actions\Mail;
 
 use App\Models\Point;
 use App\Models\Reservation;
+use App\Models\Destination;
 
 class GetMailHeaderAddressAndName
 {
     public static function run(Reservation $reservation):string
     {
+
+        /*
         if (($reservation->pickupLocation->type === Point::TYPE_ACCOMMODATION && $reservation->dropoffLocation->type === Point::TYPE_ACCOMMODATION)   ){
             return  "Valamar Riviera d.d.
                     <br>
@@ -26,6 +29,24 @@ class GetMailHeaderAddressAndName
             return "{$reservation->dropoffLocation->name}
                     <br>
                     {$reservation->dropoffLocation->address}";
+        }
+
+        return  "Valamar Riviera d.d.
+                    <br>
+                   Stancija Kaligari 1 52440 Poreč, Croatia";
+*/
+        $destination = Destination::findOrFail($reservation->destination_id);
+
+        switch ($destination->owner_id){
+            case 1:
+                return  "Valamar Riviera d.d.
+                    <br>
+                   Stancija Kaligari 1 52440 Poreč, Croatia";
+                break;
+            case 2:
+                return "Imperial Riviera d.d.
+                    <br>
+                    Ul. Jurja Barakovića 2, 51280, Rab, Croatia";
         }
 
         return  "Valamar Riviera d.d.
