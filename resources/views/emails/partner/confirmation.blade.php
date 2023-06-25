9fcec38191d0509805225430c1c6651919ed9f33
Toni Njiric <toni.njiric@ez-booker.com>
2023-06-25 20:34:48 +0200

    Update confirmation.blade.php

--- resources/views/emails/partner/confirmation.blade.php
+++ resources/views/emails/partner/confirmation.blade.php
@@ -1,21 +1,11 @@
-<x-mail.layouts.main>
-    <x-mail.body>
-        <x-mail.logo-mail-body/>
+<div style="padding: 25px 0">
+    <p>Postovani,</p>
+        <br>
+    <p>
+        U prilogu nalazi se potvrda rezervacije transfera.
+        Hvala Vam na suradnji.
+        </p>
+        <br>
 
-        <x-mail.row>
-
-
-        <div style="padding: 25px 0">
-        <p>Postovani,</p>
-            <br>
-        <p>
-            U prilogu nalazi se potvrda rezervacije transfera.
-            Hvala Vam na suradnji.
-            </p>
-            <br>
-
-            <p>Vas Valamar tim</p>
-        </div>
-    </x-mail.row>
-    </x-mail.body>
-</x-mail.layouts.main>
+        <p>Vas Valamar tim</p>
+</div>
