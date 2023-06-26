5c47a32fa2277756179e7decb5e17c5866427e5b
Toni Njiric <toni.njiric@ez-booker.com>
2023-06-26 10:16:39 +0200

    Update warning.blade.php

--- resources/views/emails/vec/warning.blade.php
+++ resources/views/emails/vec/warning.blade.php
@@ -1,11 +1,15 @@
 <div style="padding: 25px 0">
-    <p>Postovani,</p>
+    <p>Poštovani,</p>
         <br>
     <p>
-        Molimo da se obrati pozornost na rezervaciju {{ $reservation->id }}
-        Hvala Vam na suradnji.
-        </p>
-        <br>
+        Molimo da se obrati pozornost na rezervaciju #{{ $reservation->id }} - {{ $reservation->leadTraveller->full_name }} ().<br>
 
-        <p>Vas Valamar tim</p>
+        Modifikacija rezervacije sadrži više osoba, nego što je podržano za transfer partnera {{$reservation->partner->name}} - {{ $reservation->Transfer->name }}.
+
+        Za vozilo kojim se obavlja transfer {{$reservation->partner->name}} - {{ $reservation->Transfer->name }}, maksimalni kapacitet je {{ $reservation->Transfer->Vehicle->max_occ }}, dok je modifikacijom rezervacije broj osoba za koje je potreban transfer: {{ $reservation->getNumPassangersAttribute() }}.
+        </p>
+
+    <br>
+
+        <p>Valamar Transfer App</p>
 </div>
