879804c2a7e901283d77848bcd6aa2587df07221
Toni Njirić <toni.njiric@ez-booker.com>
2023-07-24 10:02:34 +0200

    Update cancellation.blade.php

--- resources/views/emails/partner/cancellation.blade.php
+++ resources/views/emails/partner/cancellation.blade.php
@@ -1,17 +1,9 @@
-<x-mail.layouts.main>
-    <x-mail.body>
-        <x-mail.logo-mail-body/>
+<div style="padding: 25px 0">
+    <p>Poštovani,
+    </p>
+    <br>
+    <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
+    <br>
+    <p> Vaš Valamar Tim</p>
+</div>
 
-
-        <x-mail.row>
-            <div style="padding: 25px 0">
-                <p>Poštovani,
-                </p>
-                <br>
-                <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
-                <br>
-                <p> Vaš Valamar Tim</p>
-            </div>
-        </x-mail.row>
-    </x-mail.body>
-</x-mail.layouts.main>
