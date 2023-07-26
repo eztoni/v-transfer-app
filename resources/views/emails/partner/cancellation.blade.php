d4bacc293d910fc2d5d9ef9bd290dcc7873b7560
Toni Njirić <toni.njiric@ez-booker.com>
2023-07-26 11:26:32 +0200

    Update cancellation.blade.php

--- resources/views/emails/partner/cancellation.blade.php
+++ resources/views/emails/partner/cancellation.blade.php
@@ -1,4 +1,94 @@
-            <div style="padding: 25px 0;font-size:14px !important;word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
+
+            <style type="text/css">
+                .page-break {
+                    page-break-after: always;
+                }
+                #outlook a {
+                    padding: 0;
+                }
+
+                body {
+                    margin: 0;
+                    padding: 0;
+                    -webkit-text-size-adjust: 100%;
+                    -ms-text-size-adjust: 100%;
+                }
+
+                table,
+                td {
+                    border-collapse: collapse;
+                    mso-table-lspace: 0pt;
+                    mso-table-rspace: 0pt;
+                }
+
+                img {
+                    border: 0;
+                    height: auto;
+                    line-height: 100%;
+                    outline: none;
+                    text-decoration: none;
+                    -ms-interpolation-mode: bicubic;
+                }
+
+                p {
+                    display: block;
+                    margin: 0px;
+                    font-weight: normal!important;
+                }
+
+                *{
+                    font-family:"Source Sans Pro",'Roboto-bold',"DeJaVu Sans";
+                    font-size: 14px !important;
+                    color: #363636;
+                }
+
+                @slot('head_after')
+
+            </style>
+            <!--[if mso]>
+            <noscript>
+                <xml>
+                    <o:OfficeDocumentSettings>
+                        <o:AllowPNG/>
+                        <o:PixelsPerInch>96</o:PixelsPerInch>
+                    </o:OfficeDocumentSettings>
+                </xml>
+            </noscript>
+            <![endif]-->
+            <!--[if lte mso 11]>
+            <style type="text/css">
+                .mj-outlook-group-fix { width:100% !important; }
+            </style>
+            <![endif]-->
+            <style type="text/css">
+                @media only screen and (min-width:480px) {
+                    .mj-column-per-100 {
+                        width: 100% !important;
+                        max-width: 100%;
+                    }
+                }
+            </style>
+            <style media="screen and (min-width:480px)">
+                .moz-text-html .mj-column-per-100 {
+                    width: 100% !important;
+                    max-width: 100%;
+                }
+            </style>
+            <style type="text/css">
+                @media only screen and (max-width:480px) {
+                    table.mj-full-width-mobile {
+                        width: 100% !important;
+                    }
+
+                    td.mj-full-width-mobile {
+                        width: auto !important;
+                    }
+                }
+            </style>
+            </head>
+
+            <body style="word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
+            <div style="padding: 25px 0;">
                 <p>Poštovani,
                 </p>
                 <br>
@@ -6,3 +96,6 @@
                 <br>
                 <p> Vaš Valamar Tim</p>
             </div>
+            </body>
+
+            </html>
