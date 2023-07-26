edc546fe3aca4548ff6edbf95adef6165088d6b8
Toni Njirić <toni.njiric@ez-booker.com>
2023-07-26 11:09:14 +0200

    Update main.blade.php

--- resources/views/components/mail/layouts/main.blade.php
+++ resources/views/components/mail/layouts/main.blade.php
@@ -6,11 +6,6 @@
     <style>
         @page { margin: 0; }
     </style>
-
-
-    @vite('resources/css/app.css')
-    @vite('resources/css/styles.scss')
-
 </head>
 
 <body style="word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
