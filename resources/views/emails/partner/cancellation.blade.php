3b48d3c6d6310668380ae96be778f2ae72b1286a
Toni Njirić <toni.njiric@ez-booker.com>
2023-07-26 10:24:45 +0200

    Revert "Update cancellation.blade.php"
    This reverts commit 879804c2a7e901283d77848bcd6aa2587df07221.

--- resources/views/emails/partner/cancellation.blade.php
+++ resources/views/emails/partner/cancellation.blade.php
@@ -1,9 +1,17 @@
-<div style="padding: 25px 0">
-    <p>Poštovani,
-    </p>
-    <br>
-    <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
-    <br>
-    <p> Vaš Valamar Tim</p>
-</div>
+<x-mail.layouts.main>
+    <x-mail.body>
+        <x-mail.logo-mail-body/>
 
+
+        <x-mail.row>
+            <div style="padding: 25px 0">
+                <p>Poštovani,
+                </p>
+                <br>
+                <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
+                <br>
+                <p> Vaš Valamar Tim</p>
+            </div>
+        </x-mail.row>
+    </x-mail.body>
+</x-mail.layouts.main>
