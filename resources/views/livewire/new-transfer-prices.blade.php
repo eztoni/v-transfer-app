ba1d64509aa4e2c5be33fddec5fbf490ec9b0c12
Toni Njirić <toni.njiric@ez-booker.com>
2023-07-31 15:08:14 +0200

    Update new-transfer-prices.blade.php

--- resources/views/livewire/new-transfer-prices.blade.php
+++ resources/views/livewire/new-transfer-prices.blade.php
@@ -29,7 +29,7 @@
                             wire:model="transferId"
                         />
                     @endif
-                    
+
                     @if ($transfer && $partner)
                         <x-native-select
                             label="Partner:"
@@ -250,17 +250,19 @@
                 @else
                     <tr>
                         <td colspan="999">
-                            <div class="ds-alert ds-alert-warning">
-                                <div class="flex-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         class="w-6 h-6 mx-2 stroke-current">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
-                                    </svg>
-                                    <label>{{$partnerMessage}}</label>
+                            <div class="flex flex-col justify-end gap-4 rounded p-4 bg-white shadow-lg border border-primary-500 pb-8">
+                                <div class="ds-alert ds-alert-warning">
+                                    <div class="flex-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             class="w-6 h-6 mx-2 stroke-current">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
+                                        </svg>
+                                        <label>{{$partnerMessage}}</label>
+                                    </div>
                                 </div>
                             </div>
-                        </TD>
+                        </td>
                     </tr>
                 @endif
             @else
