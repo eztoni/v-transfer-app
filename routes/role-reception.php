7bf25cadc106390ca935c11210b67e9f29435dc5
Toni Njirić <toni.njiric@ez-booker.com>
2023-08-01 13:53:56 +0200

    + update download

--- routes/role-reception.php
+++ routes/role-reception.php
@@ -16,3 +16,51 @@ use App\Http\Livewire\ReservationDetails;
 
 
 Route::get('/partner-daily', PartnerDaily::class)->name('partner-daily');
+
+Route::get('preview_partner_mail_list/{accommodation}/{date_from}/{date_to}',function ($accommodation,$date_from,$date_to){
+
+
+    $reservations = Reservation::query()
+        ->whereIsMain(true)
+        ->with(['leadTraveller', 'pickupLocation', 'dropoffLocation', 'returnReservation'])
+        ->where('status',Reservation::STATUS_CONFIRMED)
+        ->where(function ($q) use($date_from,$date_to) {
+            $q->where(function ($q) use($date_from,$date_to){
+                $q->whereDate('date_time', '>=', $date_from)
+                    ->whereDate('date_time', '<=',  $date_to);
+            })->orWHereHas('returnReservation',function ($q)use($date_from,$date_to){
+                $q->whereDate('date_time', '>=',  $date_from)
+                    ->whereDate('date_time', '<=',  $date_to);
+            });
+        })->where(function($q) use($accommodation) {
+            $q->where('pickup_address_id',$accommodation)
+                ->orWhere('dropoff_address_id',$accommodation);
+        })->get();
+
+
+    $accommodation_name = Point::findOrFail($accommodation)->name;
+
+    return  \App\Actions\Attachments\GenerateTransferOverviewPDF::generate(\Carbon\Carbon::make($date_from),\Carbon\Carbon::make($date_to),$reservations,$accommodation_name)->download('reception_report.pdf');
+});
+
+Route::get('download_document/{type}/{reservation_id}',function($type,$reservation_id){
+
+    $reservation = Reservation::findOrFail($reservation_id);
+    $file_name = 'BookingConfirmation'.$reservation_id.'.pdf';
+
+    switch ($type){
+        case 'booking-confirmation':
+            $view = 'attachments.booking_confirmation';
+            break;
+        case 'booking-cancellation':
+            $view = 'attachments.booking_cancellation';
+            $file_name = 'BookingCancellation'.$reservation_id.'.pdf';
+            break;
+        case 'booking-cancellation-fee':
+            $view = 'attachments.booking_cancellation_fee';
+            $file_name = 'BookingCancellationFee'.$reservation_id.'.pdf';
+            break;
+    }
+
+    return \Barryvdh\DomPDF\Facade\Pdf::loadView($view, ['reservation'=> $reservation])->download($file_name);
+});
