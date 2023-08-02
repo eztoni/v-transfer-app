e1f60e72669d6bfcd49845f266959d89fc1ffd85
Toni Njirić <toni.njiric@ez-booker.com>
2023-08-02 07:04:32 +0200

    Update cancellation.blade.php

--- resources/views/emails/partner/cancellation.blade.php
+++ resources/views/emails/partner/cancellation.blade.php
@@ -1,101 +1,123 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
-            <style type="text/css">
-                .page-break {
-                    page-break-after: always;
-                }
-                #outlook a {
-                    padding: 0;
-                }
+<head>
 
-                body {
-                    margin: 0;
-                    padding: 0;
-                    -webkit-text-size-adjust: 100%;
-                    -ms-text-size-adjust: 100%;
-                }
+    <title>
+    </title>
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        @page { margin: 0; }
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
-                table,
-                td {
-                    border-collapse: collapse;
-                    mso-table-lspace: 0pt;
-                    mso-table-rspace: 0pt;
-                }
+    @vite('resources/css/app.css')
+    @vite('resources/css/styles.scss')
 
-                img {
-                    border: 0;
-                    height: auto;
-                    line-height: 100%;
-                    outline: none;
-                    text-decoration: none;
-                    -ms-interpolation-mode: bicubic;
-                }
+    <style type="text/css">
+        .page-break {
+            page-break-after: always;
+        }
+        #outlook a {
+            padding: 0;
+        }
 
-                p {
-                    display: block;
-                    margin: 0px;
-                    font-weight: normal!important;
-                }
+        body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
 
-                *{
-                    font-family:"Source Sans Pro",'Roboto-bold',"DeJaVu Sans";
-                    font-size: 14px !important;
-                    color: #363636;
-                }
+        table,
+        td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
 
-                @slot('head_after')
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
 
-            </style>
-            <!--[if mso]>
-            <noscript>
-                <xml>
-                    <o:OfficeDocumentSettings>
-                        <o:AllowPNG/>
-                        <o:PixelsPerInch>96</o:PixelsPerInch>
-                    </o:OfficeDocumentSettings>
-                </xml>
-            </noscript>
-            <![endif]-->
-            <!--[if lte mso 11]>
-            <style type="text/css">
-                .mj-outlook-group-fix { width:100% !important; }
-            </style>
-            <![endif]-->
-            <style type="text/css">
-                @media only screen and (min-width:480px) {
-                    .mj-column-per-100 {
-                        width: 100% !important;
-                        max-width: 100%;
-                    }
-                }
-            </style>
-            <style media="screen and (min-width:480px)">
-                .moz-text-html .mj-column-per-100 {
-                    width: 100% !important;
-                    max-width: 100%;
-                }
-            </style>
-            <style type="text/css">
-                @media only screen and (max-width:480px) {
-                    table.mj-full-width-mobile {
-                        width: 100% !important;
-                    }
+        p {
+            display: block;
+            margin: 0px;
+            font-weight: normal!important;
+        }
 
-                    td.mj-full-width-mobile {
-                        width: auto !important;
-                    }
-                }
-            </style>
-            </head>
+        *{
+            font-family:"Source Sans Pro",'Roboto-bold',"DeJaVu Sans";
+            line-height: 14px;
+            color: #363636;
+        }
 
-            <body style="word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
-            <div style="padding: 25px 0;">
-                <p>Poštovani,
-                </p>
-                <br>
-                <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
-                <br>
-                <p> Vaš Valamar Tim</p>
-            </div>
-            </body>
+        @slot('head_after')
 
-            </html>
+    </style>
+    <!--[if mso]>
+    <noscript>
+        <xml>
+            <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+        </xml>
+    </noscript>
+    <![endif]-->
+    <!--[if lte mso 11]>
+    <style type="text/css">
+        .mj-outlook-group-fix { width:100% !important; }
+    </style>
+    <![endif]-->
+    <style type="text/css">
+        @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+                width: 100% !important;
+                max-width: 100%;
+            }
+        }
+    </style>
+    <style media="screen and (min-width:480px)">
+        .moz-text-html .mj-column-per-100 {
+            width: 100% !important;
+            max-width: 100%;
+        }
+    </style>
+    <style type="text/css">
+        @media only screen and (max-width:480px) {
+            table.mj-full-width-mobile {
+                width: 100% !important;
+            }
+
+            td.mj-full-width-mobile {
+                width: auto !important;
+            }
+        }
+    </style>
+</head>
+
+<body style="word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
+    <div style="padding: 25px 0;">
+        <p>Poštovani,
+        </p>
+        <br>
+        <p> U nastavku nalazi se potvrda o otkazu rezervacije.</p>
+        <br>
+        <p> Vaš Valamar Tim</p>
+    </div>
+</body>
+
+</html>
