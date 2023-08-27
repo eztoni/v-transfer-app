9ddba7519e47b2cb895ac4d6f44a3b04b111806f
Toni Njirić <toni.njiric@ez-booker.com>
2023-08-27 12:15:35 +0200

    + modification email update

--- resources/views/emails/partner/modification.blade.php
+++ resources/views/emails/partner/modification.blade.php
@@ -1,21 +1,120 @@
-<x-mail.layouts.main>
-    <x-mail.body>
-        <x-mail.logo-mail-body/>
+<head>
 
+    <title>
+    </title>
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        @page { margin: 0; }
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
-        <x-mail.row>
-            <div style="padding: 25px 0">
-                <p>Poštovani,
+    @vite('resources/css/app.css')
+    @vite('resources/css/styles.scss')
 
-                </p>
-                <br>
-                <p>
-                    U privitku nalazi se modificirana potvrda rezervacije transfera.
-                </p>
-                <br>
+    <style type="text/css">
+        .page-break {
+            page-break-after: always;
+        }
+        #outlook a {
+            padding: 0;
+        }
 
-                <p> Vaš Valamar tim</p>
-            </div>
-        </x-mail.row>
-    </x-mail.body>
-</x-mail.layouts.main>
+        body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
+
+        table,
+        td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
+
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
+
+        p {
+            display: block;
+            margin: 0px;
+            font-weight: normal!important;
+        }
+
+        *{
+            font-family:"Source Sans Pro",'Roboto-bold',"DeJaVu Sans";
+            line-height: 14px;
+            color: #363636;
+        }
+
+    </style>
+    <!--[if mso]>
+    <noscript>
+        <xml>
+            <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+        </xml>
+    </noscript>
+    <![endif]-->
+    <!--[if lte mso 11]>
+    <style type="text/css">
+        .mj-outlook-group-fix { width:100% !important; }
+    </style>
+    <![endif]-->
+    <style type="text/css">
+        @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+                width: 100% !important;
+                max-width: 100%;
+            }
+        }
+    </style>
+    <style media="screen and (min-width:480px)">
+        .moz-text-html .mj-column-per-100 {
+            width: 100% !important;
+            max-width: 100%;
+        }
+    </style>
+    <style type="text/css">
+        @media only screen and (max-width:480px) {
+            table.mj-full-width-mobile {
+                width: 100% !important;
+            }
+
+            td.mj-full-width-mobile {
+                width: auto !important;
+            }
+        }
+    </style>
+</head>
+
+<body style="word-spacing:normal;background-color:#F4F4F4;padding-top: 40px;padding-bottom: 40px">
+<p>Poštovani,
+
+</p>
+<br>
+<p>
+    U privitku nalazi se modificirana potvrda rezervacije transfera.
+</p>
+<br>
+
+<p> Vaš Valamar tim</p>
+</body>
+
+</html>
