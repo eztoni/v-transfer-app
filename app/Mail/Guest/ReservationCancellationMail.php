ca0677ccd8f843720515206fcf9c052b86bb627d
Toni Njirić <toni.njiric@ez-booker.com>
2023-08-27 11:49:02 +0200

    Update ReservationCancellationMail.php

--- app/Mail/Guest/ReservationCancellationMail.php
+++ app/Mail/Guest/ReservationCancellationMail.php
@@ -33,7 +33,7 @@ class ReservationCancellationMail extends Mailable
 
         if($this->reservation->hasCancellationFee()){
             $pdf_cf = PDF::loadView('attachments.booking_cancellation_fee',['reservation'=>$this->reservation]);
-            $this->attachData($pdf->output(),"{$booking_cancellation_fee}_{$reservation_id}.pdf");
+            $this->attachData($pdf_cf->output(),"{$booking_cancellation_fee}_{$reservation_id}.pdf");
         }
     }
 
