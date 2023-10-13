1713cdb7e3363271289b6714cb2c38c6c5b6b0b5
Toni Njirić <toni.njiric@ez-booker.com>
2023-10-13 07:34:37 +0200

    + new dashboard with reports

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -1,39 +1,142 @@
 <?php
-/**
- * Created by PhpStorm
- * User: Tin Modrić
- * Date: 10/13/2021
- */
 
 namespace App\Http\Livewire;
 
-
 use App\Models\Destination;
-use App\Scopes\CompanyScope;
-use App\Scopes\DestinationScope;
-use Carbon\Carbon;
-use Debugbar;
-use Illuminate\Database\QueryException;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Response;
+use App\Models\Partner;
+use App\Models\Point;
+use App\Models\Reservation;
 use Livewire\Component;
-use Spatie\Activitylog\Models\Activity;
 use WireUi\Traits\Actions;
-use function Symfony\Component\String\b;
 
 class Dashboard extends Component
 {
-use Actions;
+    use Actions;
 
+    public $destinationId = null;
+    public $partnerId = null;
+    public $operaErrorBookings = false;
+    public $fiscalizationErrorBookings = false;
 
-    public function mount()
+    public string $from;
+    public string $to;
+    public ?string $search = null;
+    public ?string $bookingId = null;
+    public ?string $pointId = null;
+
+    public function mount(): void
     {
+        $this->from = now()->startOfMonth();
+        $this->to = now()->endOfMonth();
     }
+
+    public function getReservationsProperty(): array|\Illuminate\Database\Eloquent\Collection
+    {
+        return Reservation::with(['leadTraveller','pickupLocation'])
+            ->when($this->partnerId,function ($q){
+                return $q->where('partner_id',$this->partnerId);
+            })
+            ->when($this->destinationId,function ($q){
+                return $q->where('destination_id',$this->destinationId);
+            })
+            ->when($this->destinationId,function ($q){
+                return $q->where('destination_id',$this->destinationId);
+            })
+            ->when($this->pointId,function ($q){
+                return $q->where(function($q){
+                    $q->where('dropoff_address_id',$this->pointId)
+                        ->orWhere('pickup_address_id',$this->pointId);
+                });
+            })
+            ->when($this->bookingId,function ($q){
+                return $q->where('id', '=',  $this->bookingId );
+            })
+            ->when($this->search, function ($q){
+                $q->where(function ($q){
+                    $q->where('pickup_address', 'LIKE', '%' . $this->search . '%')
+                        ->where('dropoff_address', 'LIKE', '%' . $this->search . '%')
+                        ->orWhereHas('leadTraveller', function ($q){
+                            $q->where('first_name', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('last_name', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('full_name', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('email', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('reservation_number', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('reservation_opera_confirmation', 'LIKE', '%' . $this->search . '%')
+                                ->orWhere('reservation_opera_id', 'LIKE', '%' . $this->search . '%');
+                        })
+                        ->orWhereHas('destination', function ($q){
+                            $q->where('name', 'LIKE', '%' . $this->search . '%');
+                        });
+                });
+            })
+            ->where('is_main',true)
+            ->orderBy('created_at','desc')
+            ->get();
+    }
+
+    public function getErrorBookings() : void {
+
+        $bookings = $this->getReservationsProperty();
+
+        if($this->operaErrorBookings === false) {
+            $this->operaErrorBookings = array();
+        }
+
+        if($this->fiscalizationErrorBookings === false) {
+            $this->fiscalizationErrorBookings = array();
+        }
+
+        foreach($bookings as $booking){
+
+            if($booking->included_in_accommodation_reservation == 1 || $booking->v_level_reservation == 1){
+               continue;
+            }
+
+            if(!$booking->isSyncedWithOpera()){
+                $this->operaErrorBookings[] = $booking;
+            }else{
+                if(!$booking->getInvoiceData('jir')){
+                    $this->fiscalizationErrorBookings[] = $booking;
+                }
+            }
+        }
+
+    }
+
+    public function has_errors(){
+
+        $this->getErrorBookings();
+
+        $return = false;
+
+        if(!empty($this->operaErrorBookings)){
+            return true;
+        }
+
+        if(!empty($this->fiscalizationErrorBookings)){
+            return true;
+        }
+
+        return $return;
+    }
+
+    public function get_opera_error_bookings(): array|\Illuminate\Database\Eloquent\Collection {
+        return $this->operaErrorBookings;
+    }
+
+    public function get_fiscalization_error_bookings(): array|\Illuminate\Database\Eloquent\Collection {
+        return $this->fiscalizationErrorBookings;
+    }
+    public function search(){
+        // Just rerender
+    }
+
 
     public function render()
     {
-        return view('livewire.dashboard');
+        $partners = Partner::all();
+        $destinations = Destination::all();
+        $points = Point::notCity()->get();
+        return view('livewire.dashboard',compact('partners','destinations','points'));
     }
 }
