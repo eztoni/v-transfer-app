85434e18ef8c4b7e322da780a38d5296ea0871c2
Toni Njirić <toni.njiric@ez-booker.com>
2023-10-26 08:04:22 +0200

    + up

--- resources/views/emails/vec/alert.blade.php
+++ resources/views/emails/vec/alert.blade.php
@@ -6,12 +6,12 @@
         </p>
         @if(!empty($alert_list['missing_reservation_number']))
             Rezervacijama iz liste niže nedostaje <b>Reservation Number</b>,ne mogu biti automatski mapirane,te time nikada ni proslijeđena u Operu.<br/>
-    
-            Za ove rezervacije potrebno je u pregledu rezervacija unijeti ispravan reservation number ( PHCode itd ).
+
+            Slijedeće Rezervacije smještaja nemaju pravilan PH broj, potrebno provjeriti transfere niže:
 
             <ul>
             @foreach($alert_list['missing_reservation_number'] as $booking)
-                    <li>{{$booking->id}} - {{$booking->leadTraveller->full_name}}</li>
+                    <li>#{{$booking->id}} - {{$booking->leadTraveller->full_name}}</li>
             @endforeach
             </ul>
 
@@ -21,7 +21,7 @@
         @if(!empty($alert_list['not_synced']))
             Rezervacije iz liste niže imaju reservation number, ali im nedostaju <b>Opera ID i Opera Confirmation.</b><br/>
 
-            Za ove rezervacije, sustav nije mogao iz MDP-a povući rezervaciju po postavljenom reservation numberu, te je isti potrebno provjeriti.
+            Ove rezervacije smještaja se nisu spustile u Operu. Potrebno provjeriti transfere niže:
 
             <ul>
                 @foreach($alert_list['not_synced'] as $booking)
@@ -35,9 +35,10 @@
 
             Rezervacije iz liste niže imaju <b>imaju sve podatke</b> no iz nedefiniranog razloga, nisu mogle biti spuštene u Operu.
 
-            Za ove rezervacije, potrebno je provjeriti Opera Sync Logove dostupne na pregledu individualne rezervacije.
+           Ove rezervacije smještaja se nisu spustile u Operu. Potrebno provjeriti transfere niže:
 
-            <ul>
+
+        <ul>
                 @foreach($alert_list['has_data_failed_sync'] as $booking)
                     <li>{{$booking->id.' ('.$booking->getAccommodationReservationCode().')'}} - {{$booking->leadTraveller->full_name}}</li>
                 @endforeach
--- resources/views/emails/vec/warning.blade.php
+++ resources/views/emails/vec/warning.blade.php
@@ -4,9 +4,8 @@
     <p>
         Molimo da se obrati pozornost na rezervaciju #<b>{{ $reservation->id }} - {{ $reservation->leadTraveller->full_name }}. ( {{ $reservation->date_time->format('d.m.Y') }} )</b><br>
 
-        Modifikacija rezervacije sadrži više osoba, nego što je podržano za transfer partnera <b>{{$reservation->partner->name}} - {{ $reservation->Transfer->name }}.</b><br/>
+        U rezervaciji smještaja promijenjen je broj osoba, potrebno je provjeriti transfer #broj transfera. {{ $reservation->id }}.</b><br/>
 
-        Za vozilo kojim se obavlja transfer {{$reservation->partner->name}} - {{ $reservation->Transfer->name }},<b style="color:green"> maksimalni kapacitet je {{ $reservation->Transfer->Vehicle->max_occ }}</b>, dok je modifikacijom rezervacije broj osoba za koje je potreban transfer: <b style="color:red">{{ $reservation->getNumPassangersAttribute() }}</b>.
         </p>
         <p>Valamar Transfer App</p>
 </div>
